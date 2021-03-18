c769ba4ad0d78fffedf8b0b951f1c965d34ba3da
Emmanuel Popoola <olaayo10@gmail.com>
2021-03-18 15:59:58 +0100

    fix eslint

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default (req, res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(201).json({ name: 'John Doe' });
 };
