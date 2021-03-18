f8c272e54a0cfe993a52bdf841aab936799057d9
Emmanuel Popoola <olaayo10@gmail.com>
2021-03-18 15:49:34 +0100

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (req, res: NextApiResponse) => {
   res.status(201).json({ name: 'John Doe' });
 };
